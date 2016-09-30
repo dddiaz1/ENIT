5c2abd111b80d4518259dd88759ec71e28692cc3
Hugo Gutierrez <arnoldohugo@gmail.com>
2016-09-30 19:32:35 +0000

    Controlador Sessions y funciones

---
+++ ENIT/application/controllers/Sessions.php
@@ -0,0 +1,14 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Projects extends CI_Controller {
+    
+    function login(){
+        
+    }
+    function logout(){
+        
+    }
+    
+}
+?>
