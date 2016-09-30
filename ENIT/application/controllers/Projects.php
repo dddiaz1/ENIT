de801120b2c25997e6bb61081bb96a362a446489
Hugo Gutierrez <arnoldohugo@gmail.com>
2016-09-30 19:28:13 +0000

    funciones del controlador projects

--- ENIT/application/controllers/Projects.php
+++ ENIT/application/controllers/Projects.php
@@ -3,7 +3,33 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Projects extends CI_Controller {
     
+    // Funciones CRUD para los proyectos.
+    function nuevo(){
+        
+        
+    }
     
+    function crear(){
+        
+    }
+    
+    function modificar(){
+        
+    }
+    
+    function eliminar(){
+        
+    }
+    
+    // Funciones para mostrar proyectos. (all & single.)
+    
+    function mostarAll(){
+        
+    }
+    
+    function mostarllSingle(){
+        
+    }
     
     
 }
