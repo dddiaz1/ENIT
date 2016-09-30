f286617fa8433f77604cff343e7ca74ecd2def77
Hugo Gutierrez <arnoldohugo@gmail.com>
2016-09-30 19:27:18 +0000

    controlador usuarios

---
+++ ENIT/application/controllers/Usuarios.php
@@ -0,0 +1,31 @@
+<? php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Usuarios extends CI_Controller {
+    
+    // Funciones para el CRUD de los usuarios.
+    function nuevo(){
+        
+    }
+    
+    function crear(){
+        
+    }
+    
+    function modificar(){
+        
+    }
+    
+    function eliminar()}{
+        
+    }
+    
+    //Funciones para eventos del usuario.
+    
+    function perfil(){
+        
+    }
+    
+}
+
+?>
